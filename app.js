9416f72e90d45d72833f57a64c98008ae517cb01
Saravjot Singh <saravjotsaini@gmail.com>
2023-06-29 16:50:23 -0400

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const fs = require('fs/promises')
+const port = process.env.PORT || 3000;
+
+app.get('/load', async (req, res) => {
+    res.send('Hello World!')
+    while(true){
+        await fs.readFile('./store.json');
+    }
+    res.send({});
+  })
+
+
+app.get('/read', (req, res) => {
+  res.send({"message": "Hi from server"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
