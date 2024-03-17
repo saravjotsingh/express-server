b99545b5c12f11c58a278bb5fa67e25cd0977205
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-16 22:16:30 -0400

    change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/express-server
-/usr/bin/pm2 delete express-sever
-/usr/bin/pm2 start app.js --name express-sever
+# /usr/bin/pm2 delete express-sever
+# /usr/bin/pm2 start app.js --name express-sever
+node app.js -d
+mkdir abc.txt
