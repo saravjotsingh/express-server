743f2718f4ab13b8421c73f604f53f426a5a6ed8
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-17 19:46:21 -0400

    deployment command changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Install node.js and PM2 globally
-sudo yum update
-echo 'Installing nodejs'
-sudo yum install nodejs-legacy -y
-echo 'Installing npm'
-sudo yum install npm  -y
-echo 'Installing pm2'
-sudo npm install pm2 -g
+# sudo yum update
+# echo 'Installing nodejs'
+# sudo yum install nodejs-legacy -y
+# echo 'Installing npm'
+# sudo yum install npm  -y
+# echo 'Installing pm2'
+# sudo npm install pm2 -g
+echo 'hi'
+whoami
