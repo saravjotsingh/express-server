c6951c21d68de6be4026aa0ec3672f9ee72304e6
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-18 18:08:02 -0400

    deployment command changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Install node.js and PM2 globally
-echo 'installing curl'
-sudo apt install curl -y
-echo 'Installing nodejs'
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-echo 'moving bash'
-source ~/.bashrc
-nvm install --lts
-nvm use 20
+sudo apt-get update
+sudo apt-get  install nodejs-legacy -y
+echo 'Installing npm'
+sudo apt-get install npm  -y
+echo 'Installing pm2'
+sudo apt-get install pm2 -g
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
