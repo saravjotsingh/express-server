5b464ccd0adaadfd9919bc3ca70dd5a96251f4ab
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-18 18:31:16 -0400

    changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ sudo apt-get  install nodejs-legacy -y
 echo 'Installing npm'
 sudo apt-get install npm  -y
 echo 'Installing pm2'
-sudo apt-get install pm2 -g
+npm install pm2 -g
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
