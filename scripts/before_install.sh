6c876622aaeefceb75872e11ad47f7a3d1a387d7
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-18 18:04:42 -0400

    deployment command changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,6 @@ sudo apt install curl -y
 echo 'Installing nodejs'
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 echo 'moving bash'
-sudo source ~/.bashrc
-sudo nvm install --lts
+source ~/.bashrc
+nvm install --lts
 nvm use 20
