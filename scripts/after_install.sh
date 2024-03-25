9a590a3c4f5a887ece047457cf6a20b92247d03b
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-25 12:36:25 -0400

    changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/express-server
 echo 'running npm install'
-npm install
+sudo npm install
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 isExistApp = `pgrep pm2`
 if [[ -n  $isExistApp ]]; then
-    sudo pm2 delete express-server
+    pm2 delete express-server
 fi
