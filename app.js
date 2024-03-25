c183e0d627f188a9c7013b0781b4b5209a9762ee
Saravjot Singh <saravjotsaini@gmail.com>
2024-03-25 12:44:58 -0400

    changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const fs = require('fs/promises')
+// const fs = require('fs/promises')
 const port = process.env.PORT || 3000;
 
 app.get('/load', async (req, res) => {
     res.send('Hello World!')
     while(true){
-        await fs.readFile('./store.json');
+        // await fs.readFile('./store.json');
     }
     res.send({});
   })
